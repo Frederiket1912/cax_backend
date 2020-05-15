52f5509e356c263bb0db55ae597a754e703ca0ee
Frederiket1912 <44972163+Frederiket1912@users.noreply.github.com>
2020-05-15 11:13:08 +0200

    Removed info about users from SetupTestUsers

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -27,10 +27,10 @@ public class SetupTestUsers {
     // Also, either delete this file, when users are created or rename and add to .gitignore
     // Whatever you do DO NOT COMMIT and PUSH with the real passwords
 
-    User user = new User("user", "test123");
-    User admin = new User("admin", "test123");
-    User both = new User("user_admin", "test123");
-    User support = new User("support", "test123");
+    User user = new User("", "");
+    User admin = new User("", "");
+    User both = new User("", "");
+    User support = new User("", "");
 
     if(admin.getUserPass().equals("test")||user.getUserPass().equals("test")||both.getUserPass().equals("test"))
       throw new UnsupportedOperationException("You have not changed the passwords");
